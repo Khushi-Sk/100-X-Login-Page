1289e5b04e35b8a64850d7565320de4c630b2451
khushi_sukar <128124987+Khushi-Sk@users.noreply.github.com>
2023-10-02 18:59:24 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "twitter-blue": "#1d9bf0",
+      },
+    },
+  },
+  plugins: [],
+}
+
